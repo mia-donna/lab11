ea1251c48fe1ab0687aa28e499a94e80bb50bfcf
Mia <mia@vidops.io>
2021-01-04 15:46:05 +0000

    Step 1 of the game

--- app/Main.hs
+++ app/Main.hs
@@ -5,12 +5,39 @@ import System.Random
 
 data Coin = Head | Tail deriving (Show, Eq) 
 
+-- Type defs
+type Name = String
+type Winner = String
+
 coinFlip :: IO Coin
 coinFlip = do
+    -- use random lib to create random Bool value 
     r <- randomIO :: IO Bool
     return $ if r then Head else Tail
 
+--process :: String -> MVar String 
+process :: Name -> MVar Winner -> MVar Coin -> IO ()
+process name winner box = do 
+    c2 <- takeMVar box
+    putStrLn $ name ++ "'s turn"
+    c1 <- coinFlip 
+    putStrLn $ " -- got " ++ (show c1)
+    if c1 == c2 then 
+        putMVar winner ("Process " ++ name ++ "wins!")
+    else do 
+        putStrLn $ " -- putting coin back in the box"
+        putMVar box c2
+        threadDelay 100
+        process name winner box
+
 main :: IO ()
 main = do
-    c <- coinFlip
-    print c
+    coin <- coinFlip
+    putStrLn $ "Random coin is: " ++ (show coin)
+    box <- newMVar coin
+    winner <- newEmptyMVar
+    forkIO (process "A" winner box)
+    forkIO (process "B" winner box)
+    forkIO (process "C" winner box)
+    w <- takeMVar winner
+    putStrLn $ "THE WINNER IS " ++ w

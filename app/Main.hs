78c706ecd1dded493ff32bbb1e4ef8fbd95a2d92
Mia <mia@vidops.io>
2021-01-08 15:20:19 +0000

    all my tests - namely updates to process function

--- app/Main.hs
+++ app/Main.hs
@@ -1,35 +1,280 @@
 module Main where
 
-import Control.Concurrent
+import Control.Concurrent  ( threadDelay, forkIO , takeMVar , putMVar , newEmptyMVar , MVar , newMVar , readMVar )
+import Text.Printf ( printf )
+import Control.Monad ( forever )
+import System.IO
+import Prelude hiding (lookup)
+import Data.Map (Map, (!) )
+import Data.Set as Set
+import qualified Data.Map as Map
+import Control.Concurrent.STM
+import Data.Maybe
 import System.Random
 
-data Coin = Head | Tail deriving (Show, Eq) 
+{- chan.hs
+-- <<Stream
+type Stream a = MVar (Item a)
+data Item a   = Item a (Stream a)
+-- >>
 
--- Type defs
-type Name = String
-type Winner = String
+-- <<Chan
+data Chan a
+ = Chan (MVar (Stream a))
+        (MVar (Stream a))
+-- >>
 
-coinFlip :: IO Coin
-coinFlip = do
-    -- use random lib to create random Bool value 
-    r <- randomIO :: IO Bool
-    return $ if r then Head else Tail
+-- <<newChan
+newChan :: IO (Chan a)
+newChan = do
+  hole  <- newEmptyMVar
+  readVar  <- newMVar hole
+  writeVar <- newMVar hole
+  return (Chan readVar writeVar)
+-- >>
 
---process :: String -> MVar String 
-process :: Name -> MVar Winner -> MVar Coin -> IO ()
+-- <<writeChan
+writeChan :: Chan a -> a -> IO ()
+writeChan (Chan _ writeVar) val = do
+  newHole <- newEmptyMVar
+  oldHole <- takeMVar writeVar
+  putMVar oldHole (Item val newHole)
+  putMVar writeVar newHole
+-- >>
+
+-- <<readChan
+readChan :: Chan a -> IO a
+readChan (Chan readVar _) = do
+  stream <- takeMVar readVar            -- <1>
+  Item val tail <- takeMVar stream      -- <2>
+  putMVar readVar tail                  -- <3>
+  return val
+-- >>
+
+-- <<dupChan
+dupChan :: Chan a -> IO (Chan a)
+dupChan (Chan _ writeVar) = do
+  hole <- takeMVar writeVar
+  putMVar writeVar hole
+  newReadVar <- newMVar hole
+  return (Chan newReadVar writeVar)
+-- >>
+-- <<unGetChan
+unGetChan :: Chan a -> a -> IO ()
+unGetChan (Chan readVar _) val = do
+  newReadEnd <- newEmptyMVar             -- <1>
+  readEnd <- takeMVar readVar            -- <2>
+  putMVar newReadEnd (Item val readEnd)  -- <3>
+  putMVar readVar newReadEnd             -- <4>
+-- >>
+
+main = do
+  c <- newChan
+  writeChan c 'a'
+  readChan c >>= print
+  c2 <- dupChan c
+  writeChan c 'b'
+  readChan c >>= print
+  readChan c2 >>= print
+-}
+
+
+
+
+
+
+{-
+-- phonebook.hs 
+-- <<types
+type Name        = String
+type PhoneNumber = String
+type PhoneBook   = Map Name PhoneNumber
+
+newtype PhoneBookState = PhoneBookState (MVar PhoneBook)
+-- >>
+
+-- <<new
+new :: IO PhoneBookState
+new = do
+  m <- newMVar Map.empty
+  return (PhoneBookState m)
+-- >>
+
+-- <<insert
+insert :: PhoneBookState -> Name -> PhoneNumber -> IO ()
+insert (PhoneBookState m) name number = do
+  book <- takeMVar m
+  putMVar m (Map.insert name number book)
+-- >>
+
+-- <<lookup
+lookup :: PhoneBookState -> Name -> IO (Maybe PhoneNumber)
+lookup (PhoneBookState m) name = do
+  book <- takeMVar m
+  putMVar m book
+  return (Map.lookup name book)
+-- >>
+
+-- <<main
+main = do
+  s <- new
+  sequence_ [ insert s ("name" ++ show n) (show n) | n <- [1..10000] ]
+  lookup s "name8" >>= print
+  lookup s "unknown" >>= print
+-- >>
+-}
+
+
+{- logger.hs
+-- <<Logger - MVar that we use for communication with the logging thread that takes a logcommand 
+-- requests are made by placing a logcommand in the MVar
+-- and the logging thread will process them one at a time by taking them from the MVar
+data Logger = Logger (MVar LogCommand)
+
+-- Two commands that we can make, so String takes the message and Stop represents the message requesting the logging thread to terminate
+data LogCommand = Message String | Stop (MVar ())
+-- >>
+
+-- <<initLogger creates a new logging service
+initLogger :: IO Logger
+initLogger = do
+  m <- newEmptyMVar
+  let l = Logger m
+  forkIO (logger l)
+  return l
+-- >>
+
+-- <<logger once we've created an empty MVar and forked a thread to perform the service -
+-- the thread will run the function logger:
+logger :: Logger -> IO ()
+logger (Logger m) = loop
+ where
+  loop = do
+    cmd <- takeMVar m
+    case cmd of
+      Message msg -> do
+        putStrLn msg
+        loop
+      Stop s -> do
+        putStrLn "logger: stop"
+        putMVar s ()
+-- >>
+
+-- <<logMessage is a function that the client uses to log a message
+logMessage :: Logger -> String -> IO ()
+logMessage (Logger m) s = putMVar m (Message s)
+-- >>
+
+-- <<logStop
+logStop :: Logger -> IO ()
+logStop (Logger m) = do
+  s <- newEmptyMVar
+  putMVar m (Stop s)
+  takeMVar s
+-- >>
+
+-- <<main
+main :: IO ()
+main = do
+  l <- initLogger
+  logMessage l "hello"
+  logMessage l "bye"
+  logStop l
+                          
+-- >>
+
+-}
+
+{-
+--mvar 3
+main :: IO ()
+main = do
+  m <- newEmptyMVar
+  takeMVar m
+-}
+
+{-
+--mvar2.hs
+main :: IO ()
+main = do
+  m <- newEmptyMVar
+  forkIO $ do putMVar m 'x'; putMVar m 'y'
+  r <- takeMVar m
+  print r
+  r <- takeMVar m
+  print r
+-}
+
+{- mvar1.hs
+-- <<main
+main :: IO ()
+main = do
+  m <- newEmptyMVar
+  forkIO $ putMVar m 'x'
+  r <- takeMVar m
+  print r
+-- >>
+-}
+
+
+{--- reminder.hs (with 2 updates - recursive loop on main -- we can terminate the program even if there are outstanding reminders)
+main :: IO ()
+main = loop
+  where 
+    loop = do
+     s <- getLine           -- <1>  Waits for input from the user
+     if s == "exit" 
+        then return ()
+        else do forkIO $ setReminder s -- <2>  Creates a new thread to handle this reminder
+                loop
+
+setReminder :: String -> IO ()
+setReminder s  = do
+  let t = read s :: Int
+  printf "Ok, I'll remind you in %d seconds\n" t
+  threadDelay (10^6 * t)                   -- <3> The new thread, after printing a confirmation message, waits for the spec seconds in threadDelay
+  printf "%d seconds is up! BING!\BEL\n" t -- <4> Finally, when threadDelay returns, the reminder message is printed
+-- >>
+-}
+
+
+{-
+-- fork.hs 
+main :: IO ()
+main = do
+  hSetBuffering stdout NoBuffering            -- <1> This puts the output HANDLE into nonbuffered mode, so that we can see the interleavign more clearly
+  forkIO (replicateM_ 100000 (putChar 'A'))   -- <2> Creates a thread to print the character A x 100000
+  replicateM_ 100000 (putChar 'B')            -- <3> In the main thread, print the character B x 100000
+-- >>
+-}
+
+{-mapIntToDice :: Int -> Dice
+mapIntToDice n = case n of
+    0 -> One
+    1 -> Two
+    2 -> Three
+    3 -> Four
+    4 -> Five
+    5 -> Six
+    where r = mod n 6-}
+
+{-
+process :: String -> MVar String -> MVar Coin -> IO () 
 process name winner box = do 
-    c2 <- takeMVar box
+    c1 <- takeMVar box
     putStrLn $ name ++ "'s turn"
-    c1 <- coinFlip 
-    putStrLn $ " -- got " ++ (show c1)
+    c2 <- coinFlip 
+    putStrLn $ " -- got " ++ (show c2)
     if c1 == c2 then 
-        putMVar winner ("Process " ++ name ++ "wins!")
+        putMVar winner $ "Process " ++ name ++ "wins!"
     else do 
         putStrLn $ " -- putting coin back in the box"
-        putMVar box c2
+        putMVar box c1
         threadDelay 100
         process name winner box
+-}
 
+{-
 main :: IO ()
 main = do
     coin <- coinFlip
@@ -40,4 +285,158 @@ main = do
     forkIO (process "B" winner box)
     forkIO (process "C" winner box)
     w <- takeMVar winner
-    putStrLn $ "THE WINNER IS " ++ w
+    putStrLn $ w
+-}
+
+{-main :: IO ()
+main = do
+    putStrLn $ "hi"
+-}
+
+
+
+
+
+{-
+-- FIRST COIN FLIP GAME WITH NOTES ******************************************************************************
+-- datatype
+data Coin = Head | Tail  deriving (Show, Eq)
+type Name = String
+type Winner = String
+
+-- function - gets one of the two values at random
+coinFlip :: IO Coin
+coinFlip = do 
+    r <- randomIO :: IO Bool
+    return $ if r then Head else Tail
+    -- if true head, if false tail
+
+-- define the functions that are going to be the implementations of our threads 
+-- three different threads and they're all playing the game 
+-- define the thread -> thread is a process/process is a thread
+-- give each thread a name of the player, then two boxes that each thread will have access to
+-- MVar String (name of winner) -- MVar Coin (coin that was initially flipped)
+-- 1) we will flip a coin, 2) then the players will take turns to try to get the same value as the initial coin
+-- needs to be IO() to spawn a thread
+-- 
+
+process :: Name-> MVar Winner -> MVar Coin -> IO ()
+process name winner box = do 
+    -- we will take a coin value from the box so that other process is blocked
+    -- if there is someone already took the coin, this thread will hang + waiting until something in the box
+    c2 <- takeMVar box 
+    -- then the thread flips its own coin 
+    putStrLn $ name ++ "'s turn"
+    c1 <- coinFlip
+    -- then we compare the two coins
+    -- each process needs to 1. TAKE A COIN 2. CHECK if its the same in the BOX 3. IF YES == WINNER and stop threads 4. IF NO the put COIN INTO BOX 
+    putStrLn $ name ++ "--- got " ++ (show c1)  
+    if c1 == c2 then
+        -- we can declare this thread the winner -- puts in the winner box the message that says "i've won"
+        putMVar winner ("Process " ++ name ++ " wins!")
+        -- if not winner then need to do a few things
+    else do 
+        -- put it back in the box so the next thread can have a go (this is where we checked to see if it was the same as our flip)
+        putStrLn $ "putting coin back in the box"
+        putMVar box c2
+        threadDelay 100 
+        -- wait a bit until we can repeat the process
+        process name winner box
+
+main :: IO ()
+main = do
+    -- Flip the original coin
+    coin <- coinFlip
+    putStrLn $ "Random coin is: " ++ (show coin)
+    -- put coin in the main box (main box is box) and starts with a value (newMVar)
+    box <- newMVar coin 
+    -- create an empty box for the winner (newEmptyMVar)
+    winner <- newEmptyMVar 
+    -- fork the three processes, with the winner box and the coin box
+    forkIO (process "A" winner box)
+    forkIO (process "B" winner box)
+    forkIO (process "C" winner box)
+    -- so that these processes can interact, before the main thread ends, block the main thread
+    -- we need a way to ensure the main thread waits so the 3 players can play
+    -- because this box is initially empty, this will block the main thread
+    -- main thread is waiting until 1 player declares themselves the winner
+    -- then the winner will be filled and the main thread can take the winner from the box
+    w <- takeMVar winner
+    putStrLn $ "The winner is: " ++ w
+-- END OF FIRST COIN FLIP GAME WITH NOTES ******************************************************************************
+
+-}
+
+
+-- SECOND COIN FLIP GAME WITH NOTES ******************************************************************************
+
+-- here i've updated the create thread function "process" which includes an original data type Account and the Account Number printed
+
+
+-- datatype
+data Coin = Head | Tail  deriving (Show, Eq)
+type Name = String
+type Winner = String
+
+-- function - gets one of the two values at random
+coinFlip :: IO Coin
+coinFlip = do 
+    r <- randomIO :: IO Bool
+    return $ if r then Head else Tail
+    -- if true head, if false tail
+
+-- define the functions that are going to be the implementations of our threads 
+-- three different threads and they're all playing the game 
+-- define the thread -> thread is a process/process is a thread
+-- give each thread a name of the player, then two boxes that each thread will have access to
+-- MVar String (name of winner) -- MVar Coin (coin that was initially flipped)
+-- 1) we will flip a coin, 2) then the players will take turns to try to get the same value as the initial coin
+-- needs to be IO() to spawn a thread
+-- 
+
+data Account = One | Two | Three deriving (Show, Eq)
+
+process :: Name -> Account -> MVar Winner -> MVar Coin -> IO ()
+process name account winner box = do 
+    -- we will take a coin value from the box so that other process is blocked
+    -- if there is someone already took the coin, this thread will hang + waiting until something in the box
+    c2 <- takeMVar box 
+    -- then the thread flips its own coin 
+    putStrLn $ name ++ "'s turn"
+    c1 <- coinFlip
+    -- then we compare the two coins
+    -- each process needs to 1. TAKE A COIN 2. CHECK if its the same in the BOX 3. IF YES == WINNER and stop threads 4. IF NO the put COIN INTO BOX 
+    putStrLn $ name ++ "--- got " ++ (show c1)  
+    if c1 == c2 then
+        -- we can declare this thread the winner -- puts in the winner box the message that says "i've won"
+        putMVar winner ("Process " ++ name ++ " wins!" ++ " Their account number is: " ++ (show account) )
+        -- if not winner then need to do a few things
+    else do 
+        -- put it back in the box so the next thread can have a go (this is where we checked to see if it was the same as our flip)
+        putStrLn $ "putting coin back in the box"
+        putMVar box c2
+        threadDelay 100 
+        -- wait a bit until we can repeat the process
+        process name account winner box
+
+main :: IO ()
+main = do
+    -- Flip the original coin
+    coin <- coinFlip
+    putStrLn $ "Random coin is: " ++ (show coin) 
+    -- put coin in the main box (main box is box) and starts with a value (newMVar)
+    box <- newMVar coin 
+    -- create an empty box for the winner (newEmptyMVar)
+    winner <- newEmptyMVar 
+    -- fork the three processes, with the winner box and the coin box
+    forkIO (process "A" One winner box)
+    forkIO (process "B" Two winner box)
+    forkIO (process "C" Three winner box)
+    -- so that these processes can interact, before the main thread ends, block the main thread
+    -- we need a way to ensure the main thread waits so the 3 players can play
+    -- because this box is initially empty, this will block the main thread
+    -- main thread is waiting until 1 player declares themselves the winner
+    -- then the winner will be filled and the main thread can take the winner from the box
+    w <- takeMVar winner
+    putStrLn $ "The winner is: " ++ w
+-- END OF SECOND COIN FLIP GAME WITH NOTES ******************************************************************************
